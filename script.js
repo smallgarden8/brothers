1821c5286d275738524badb3b12998de78e34dbf
unknown <jardinpequeno8@gmail.com>
2023-11-23 17:31:23 -0500

    primercomit

---
+++ script.js
@@ -0,0 +1,58 @@
+var swiper = new Swiper(".mySwiper-1", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable:true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    }
+
+});
+
+var swiper = new Swiper(".mySwiper-2", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+    breakpoints : {
+        0: {
+            slidesPerView: 1
+        },
+        520: {
+            slidesPerView: 2
+        },
+        950: {
+            slidesPerView: 3
+        }
+    }
+
+});
+
+window.onscroll = function(){
+    console.log(document.documentElement.scrollTop);
+    if(document.documentElement.scrollTop > 100) {
+      document.querySelector('.go-top-container').classList.add('show');
+       
+    }
+    else{
+      document.querySelector('.go-top-container').classList.remove('show');
+    }
+  }
+   
+  document.querySelector('.go-top-container').addEventListener('click', () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
+
+
+
+
